9d38d6eafac60131593cd4965d502e8711bd1404
hopezh <hope.zh@gmail.com>
2023-05-15 00:22:55 +0800

    add barbecue.nvim

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,22 @@
+return {
+    'utilyre/barbecue.nvim',
+
+    name = 'barbecue',
+
+    version = '*',
+
+    dependencies = {
+        'SmiteshP/nvim-navic',
+        'nvim-tree/nvim-web-devicons',
+    },
+
+    opts = {
+    },
+
+    config = function ()
+        require('barbecue').setup({
+        })
+    end,
+}
+
+
