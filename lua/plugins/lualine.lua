20cd57ca36a75a81d385cb8a7b93c4fe1c982994
hopezh <hope.zh@gmail.com>
2023-05-14 20:18:56 +0800

    enable icon in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         require("lualine").setup{
             options = {
-                icons_enabled = false,
+                icons_enabled = true,
                 theme = 'auto',
                 component_separators = '|',
                 section_separators = '',
