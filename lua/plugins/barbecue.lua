86e907608ba5374b265f59468364310efe606acf
hopezh <hope.zh@gmail.com>
2023-05-15 00:30:58 +0800

    add kind icons

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -16,6 +16,35 @@ return {
     config = function ()
         require('barbecue').setup({
             context_follow_icon_color = true,
+
+            kinds = {
+                File = "",
+                Module = "",
+                Namespace = "",
+                Package = "",
+                Class = "",
+                Method = "",
+                Property = "",
+                Field = "",
+                Constructor = "",
+                Enum = "",
+                Interface = "",
+                Function = "",
+                Variable = "",
+                Constant = "",
+                String = "",
+                Number = "",
+                Boolean = "",
+                Array = "",
+                Object = "",
+                Key = "",
+                Null = "",
+                EnumMember = "",
+                Struct = "",
+                Event = "",
+                Operator = "",
+                TypeParameter = "",
+              },
         })
     end,
 }
