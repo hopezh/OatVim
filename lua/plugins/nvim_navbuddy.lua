fc0721e018f79c7e419d518e85d5eec2f0e2648f
hopezh <hope.zh@gmail.com>
2023-05-16 22:24:20 +0800

    add nvim-navbuddy

---
+++ lua/plugins/nvim_navbuddy.lua
@@ -0,0 +1,40 @@
+return {
+    'SmiteshP/nvim-navbuddy',
+
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'SmiteshP/nvim-navic',
+        'MunifTanjim/nui.nvim',
+        'numToStr/Comment.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+
+    lazy = true,
+
+    keys = {
+        { "<leader>nv", "<cmd>Navbuddy<cr>", desc = "Navbuddy" },
+    },
+
+    config = function()
+        local actions = require("nvim-navbuddy.actions")
+        local navbuddy = require("nvim-navbuddy")
+        navbuddy.setup({
+          window = {
+            border = "rounded",
+            size = "80%",
+          },
+          mappings = {
+            ["k"] = actions.next_sibling,
+            ["i"] = actions.previous_sibling,
+            ["j"] = actions.parent,
+            ["l"] = actions.children,
+          },
+          lsp = {
+            auto_attach = true,
+            preference = "nvim_lsp",
+          },
+        })
+    end,
+}
+
+
