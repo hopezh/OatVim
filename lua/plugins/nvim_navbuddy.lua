7cd644eadee975c511048f2956c8468ec0f5a9c3
hopezh <hope.zh@gmail.com>
2023-05-16 22:27:33 +0800

    format

--- lua/plugins/nvim_navbuddy.lua
+++ lua/plugins/nvim_navbuddy.lua
@@ -18,17 +18,20 @@ return {
     config = function()
         local actions = require("nvim-navbuddy.actions")
         local navbuddy = require("nvim-navbuddy")
+
         navbuddy.setup({
           window = {
             border = "rounded",
             size = "80%",
           },
+
           mappings = {
             ["k"] = actions.next_sibling,
             ["i"] = actions.previous_sibling,
             ["j"] = actions.parent,
             ["l"] = actions.children,
           },
+
           lsp = {
             auto_attach = true,
             preference = "nvim_lsp",
