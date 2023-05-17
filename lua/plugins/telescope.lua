05a0efc9be599ace8e1e941c6b3ef81682c79960
hopezh <hope.zh@gmail.com>
2023-05-17 22:09:27 +0800

    tidy up telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,14 +4,6 @@
 -- 	return
 -- end
 
--- -- import telescope actions safely
--- local actions_setup, actions = pcall(require, "telescope.actions")
--- if not actions_setup then
--- 	return
--- end
-
--- require("telescope").load_extension("lazygit")
-
 return {
     "nvim-telescope/telescope.nvim",
 
@@ -29,7 +21,6 @@ return {
             -- settings
         })
     end,
-
 }
 
 
