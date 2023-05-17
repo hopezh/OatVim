57b8ecede64d72b336fc11b44bc6dd410886c11d
hopezh <hope.zh@gmail.com>
2023-05-17 18:37:50 +0800

    config flit.nvim

---
+++ after/plugin/flit.lua
@@ -0,0 +1,22 @@
+local status, flit = pcall(require, 'flit')
+if (not status)  then return end
+
+flit.setup {
+    keys = {
+        f = 'f',
+        F = 'F',
+        t = 't',
+        T = 'T',
+    },
+
+    -- A string like "nv", "nvo", "o", etc.
+    labeled_modes = "nvo",
+
+    multiline = true,
+
+    -- Like `leap`s similar argument (call-specific overrides).
+    -- E.g.: opts = { equivalence_classes = {} }
+    opts = {}
+}
+
+
