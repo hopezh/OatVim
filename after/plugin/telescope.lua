d9834777787e2e480da15dfd5019ac1c2a5b03e9
hopezh <hope.zh@gmail.com>
2023-05-18 00:34:37 +0800

    update telescope config

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,33 @@
+local telescope = require('telescope')
+
+telescope.setup({
+
+    defaults = {
+        layout_config = {
+            width = 0.75,
+            prompt_position = 'top',
+            preview_cutoff = 120,
+        },
+        color_devicons = true,
+        prompt_prefix='',
+        sorting_strategy = 'ascending',
+    },
+
+    initial_mode = 'insert',
+
+    pickers = {
+        find_files = {
+            -- theme = 'dropdown',
+            -- theme = 'cursor',
+            -- theme = 'ivy',
+        },
+
+        lsp_code_actions = {
+            theme = 'cursor',
+        },
+    },
+})
+
+require('telescope').load_extension('file_browser')
+
+
