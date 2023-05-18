262bc8d6f4387bf023d5e5183b73bbfdcbad73b7
hopezh <hope.zh@gmail.com>
2023-05-18 22:48:14 +0800

    edit zen mode

--- after/plugin/zen_mode.lua
+++ after/plugin/zen_mode.lua
@@ -5,10 +5,19 @@ zenMode.setup {
     window = {
         backdrop = 0.95,
         width = 140,
+        options = {
+            -- signcolumn = "no",
+            -- number = false,
+        },
     },
 
 }
 
-vim.keymap.set('n', '<leader>zz', '<cmd>ZenMode<cr>', { silent = true })
+vim.keymap.set(
+    'n',
+    '<leader>zz',
+    '<cmd>ZenMode<cr>',
+    { silent = true }
+)
 
 
