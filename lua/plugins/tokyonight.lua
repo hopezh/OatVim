8fc6bc211bf6e5744bf4a54be525eea564e65195
hopezh <hope.zh@gmail.com>
2023-05-20 17:22:16 +0800

    use day theme of tokyonight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,11 +6,11 @@ return {
 
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = "day", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         -- light_style = "day", -- The theme is used when the background is set to light
         -- Enable this to disable setting the background color
         -- ...when enabled, need to provide background_colour="#000000" for nvim-notify
-        transparent = true,
+        transparent = false,
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
           -- Style to be applied to different syntax groups
