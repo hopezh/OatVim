6363856e14d029439022ba4f2d4cb96431f20b1a
hopezh <hope.zh@gmail.com>
2023-05-20 22:06:29 +0800

    use night theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,7 +6,7 @@ return {
 
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = "day", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         -- light_style = "day", -- The theme is used when the background is set to light
         -- Enable this to disable setting the background color
         -- ...when enabled, need to provide background_colour="#000000" for nvim-notify
